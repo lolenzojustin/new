1f168dd4037347df7a63f10c6023a8cd052d3d81
lolenzojusin <leminhthang7896@gmail.com>
2023-08-04 06:56:21 -0700

    message

---
+++ app.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('button-id');
+const h1 = document.getElementById('h1-id');
+
+button.addEventListener('click', () => {
+  h1.innerText = 'ok';
+  button.innerText = 'clicked';
+});
+console.log(button);
